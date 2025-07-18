32afe7a223cf06fad0195731773507adf7ff7c7b
ming0511 <alsrbdhkalsw@naver.com>
2025-07-18 14:26:47 +0900

    refactor: command object 추가(Member의 login)

--- src/main/java/gift/member/service/LoginServiceImpl.java
+++ src/main/java/gift/member/service/LoginServiceImpl.java
@@ -1,7 +1,7 @@
 package gift.member.service;
 
 import gift.exception.member.LoginFailedException;
-import gift.member.dto.LoginRequestDto;
+import gift.member.dto.LoginCommand;
 import gift.member.dto.TokenResponseDto;
 import gift.member.entity.Member;
 import gift.member.repository.MemberRepository;
@@ -18,12 +18,12 @@ public class LoginServiceImpl implements LoginService {
     }
 
     @Override
-    public TokenResponseDto login(LoginRequestDto loginRequestDto) {
+    public TokenResponseDto login(LoginCommand dto) {
         // DB 조회 -> 성공 시 Token 생성, 실패 시 로그인 실패
-        Member member = members.findByEmail(loginRequestDto.email())
+        Member member = members.findByEmail(dto.email())
             .orElseThrow(() -> new LoginFailedException("이메일 또는 비밀번호가 올바르지 않습니다."));
 
-        if (!member.getPassword().equals(loginRequestDto.password())) {
+        if (!member.getPassword().equals(dto.password())) {
             // 실패 시 로그인 실패(403 Forbidden)
             throw new LoginFailedException("이메일 또는 비밀번호가 올바르지 않습니다.");
         }
