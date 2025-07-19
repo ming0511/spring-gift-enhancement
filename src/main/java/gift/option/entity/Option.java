4c6af31994bf0ac1bebd9315a5727b7599cf57f5
ming0511 <alsrbdhkalsw@naver.com>
2025-07-19 18:38:47 +0900

    feat: Option domain 구현

---
+++ src/main/java/gift/option/entity/Option.java
@@ -0,0 +1,62 @@
+package gift.option.entity;
+
+import gift.product.entity.Product;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "option")
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long optionId;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", foreignKey = @ForeignKey(name = "fk_option_product_id_ref_product_id"))
+    private Product product;
+
+    protected Option() {
+    }
+
+    public Option(Long optionId, String name, Integer quantity, Product product) {
+        this.optionId = optionId;
+        this.name = name;
+        this.quantity = quantity;
+        this.product = product;
+    }
+
+    public Long getOptionId() {
+        return optionId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public Long getProductId() {
+        return product.getProductId();
+    }
+}
