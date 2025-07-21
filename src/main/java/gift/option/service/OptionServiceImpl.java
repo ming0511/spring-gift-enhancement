b2d619e095ed5358423cc162da0edea24faf3474
ming0511 <alsrbdhkalsw@naver.com>
2025-07-21 14:38:10 +0900

    refactor: 옵션과 상품 객체 그래프 탐색 적용(OptionService.delete)

--- src/main/java/gift/option/service/OptionServiceImpl.java
+++ src/main/java/gift/option/service/OptionServiceImpl.java
@@ -83,14 +83,14 @@ public class OptionServiceImpl implements OptionService {
     @Override
     @Transactional
     public void deleteProductOption(Long productId, Long optionId) {
-        Product product = productRepository.findById(productId)
-            .orElseThrow(() -> new ProductNotFoundException("해당 상품을 찾을 수 없습니다."));
 
         Option option = optionRepository.findById(optionId)
             .orElseThrow(() -> new OptionNotFoundException("해당 옵션을 찾을 수 없습니다."));
 
-        if (!option.getProductId().equals(product.getProductId())) {
-            throw new OptionNotFoundException("해당 옵션은 지정된 상품에 속해 있지 않습니다.");
+        Product product = option.getProduct();
+
+        if (!product.getProductId().equals(productId)) {
+            throw new ProductMismatchException("옵션에 저장된 상품과 전달받은 상품 ID가 일치하지 않습니다.");
         }
 
         product.removeOption(option);
